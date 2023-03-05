fada0e0d0f56cb955c261e88051abcfcaf50e0ec
Роїк Олександр <126152318+roiksasha@users.noreply.github.com>
2023-03-05 19:38:47 +0200

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = i
+
+
+        while pos> 0 and arr[pos - 1] > cursor:
+
+            arr[pos] = arr[pos - 1]
+            pos=pos -1
+
+        arr[pos] = cursor
+
+    return arr
